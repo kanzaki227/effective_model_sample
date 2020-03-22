6daee446f448a2ec246c826337b2c73ed0707d48
kanzaki227 <mcfkzk2gh@i.softbank.jp>
2020-03-22 14:43:36 +0900

    cおみt

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,11 @@
 class Task < ApplicationRecord
+  enum kind: [:individual, :work, :others]  
+
+  scope :incoming,  -> { where('start_at > ?', Time.zone.now) }
+  # finishedカラムをtrueに更新するメソッドを定義
+  def update_finished_true
+    self.finished = true if self.finished == false
+    self.save
+  end
+
 end
